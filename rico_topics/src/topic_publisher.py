16c815d379a0e5fcc4b8a106a6d42a2bd1f343cc
Rico Picone <ricopicone@gmail.com>
2020-04-10 12:30:30 -0700

    changed queue size and added comments

--- rico_topics/src/topic_publisher.py
+++ rico_topics/src/topic_publisher.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32 # standard int
 
-rospy.init_node('topic_publisher')
-pub = rospy.Publisher('counter', Int32, queue_size=1)
+# Setup: initialize node, register topic, set rate
+rospy.init_node( # initialize node
+  'topic_publisher' # node default name
+)
+pub = rospy.Publisher( # register topic w/roscore
+  'counter', # topic name
+  Int32, # topic type
+  queue_size=5 # queue size
+)
+rate = rospy.Rate(2) # adaptive rate in Hz
 
-rate = rospy.Rate(2)
-
+# Loop: publish, count, sleep
 count = 0
-while not rospy.is_shutdown():
-    pub.publish(count)
-    count += 1
-    rate.sleep()
+while not rospy.is_shutdown(): # until ctrl-c
+    pub.publish(count) # publish count
+    count += 1 # increment
+    rate.sleep() # set by rospy.Rate above
