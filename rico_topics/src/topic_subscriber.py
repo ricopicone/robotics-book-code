2522e46bacfb7467fd234d65c1b2b383392ca7fc
Rico Picone <ricopicone@gmail.com>
2020-04-15 15:42:39 -0700

    tightened it up and added comments

--- rico_topics/src/topic_subscriber.py
+++ rico_topics/src/topic_subscriber.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
-
 import rospy
-
 from std_msgs.msg import Int32
 
+def callback(msg):  # callback for receiving messages
+  print msg.data    # print to Terminal
 
-def callback(msg):
-    print msg.data
+rospy.init_node('topic_subscriber') # initialize node
 
+sub = rospy.Subscriber('counter', Int32, callback) # subscribe
 
-rospy.init_node('topic_subscriber')
-
-sub = rospy.Subscriber('counter', Int32, callback)
-
-rospy.spin()
+rospy.spin() # wait for node to be shut down
